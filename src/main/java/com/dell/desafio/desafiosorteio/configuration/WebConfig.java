88acc08558c40216faa9afafebf40d6a496b88c4
Marlon Mariano <marlon.mariano.pb@compasso.com.br>
2024-03-20 16:54:03 -0300

    feat: adding config allowing external access

---
+++ src/main/java/com/dell/desafio/desafiosorteio/configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package com.dell.desafio.desafiosorteio.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+@Override
+public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+
+}
+
+
+}
